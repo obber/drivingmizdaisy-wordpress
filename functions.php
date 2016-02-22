438f904df453052f02cbffe1aebe1924e7eddfc3
Kan Adachi <Kadachi1023@gmail.com>
2016-02-22 11:21:36 -0800

    Cleaned up functions.php, removed comments

--- functions.php
+++ functions.php
@@ -46,15 +46,7 @@ genesis_unregister_layout( 'sidebar-sidebar-content' );
 //* Unregister secondary sidebar
 unregister_sidebar( 'sidebar-alt' );
 
-/**
- * Remove Genesis Page Templates
- *
- * @author Bill Erickson
- * @link http://www.billerickson.net/remove-genesis-page-templates
- *
- * @param array $page_templates
- * @return array
- */
+//* Remove Genesis Page Templates
 function be_remove_genesis_page_templates( $page_templates ) {
 	unset( $page_templates['page_archive.php'] );
 	unset( $page_templates['page_blog.php'] );
